42cc2a479f5cac4d5f5bb8f6cd114056ab623e49
robertbruzualpatino-oss <rbruzualpatio@byupathway.edu>
2026-01-05 13:40:13 -0400

    Made adjustments to the code.

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World!!! Welcome to the Sandbox project.");
+        Console.WriteLine("Hello World! Welcome to the Sandbox project.");
     }
 }
