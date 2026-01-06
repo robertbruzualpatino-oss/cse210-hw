b6ae6d9192a80ed87cc565727d26a008680e0fac
robertbruzualpatino-oss <rbruzualpatio@byupathway.edu>
2026-01-06 14:32:56 -0400

    Added some more line statements in exercise 2.

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,7 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        // This is exercise 2
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("This is Exercise 2 of Week 1.");
+
+        Console.Write("Enter number: ");
+        string valueFromUser = Console.ReadLine();
+
+        int x = int.Parse(valueFromUser);
+        int y = 2;
+
+        if (x > y)
+        {
+            Console.WriteLine("x is greater than y");
+        }
+        else if (x < y)
+        {
+            Console.WriteLine("x is less than y");
+        }
+        else
+        {
+            Console.WriteLine("x is equal to y");
+        }
     }
 }
