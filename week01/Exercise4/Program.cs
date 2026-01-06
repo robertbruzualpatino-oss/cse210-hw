71850704b8bae018ad2be8a6bc9955623899eba0
robertbruzualpatino-oss <rbruzualpatio@byupathway.edu>
2026-01-06 19:54:40 -0400

    I've added some more line statements in exercise 4.

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -4,7 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        // This is exercise 4
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("This is Exercise 4 of Week 1.");
+
+        List<int> numbers = new List<int> { 1, 2, 3, 4, 5 };
+        List<string> words = new List<string> { "apple", "banana", "cherry" };
+
+        List<string> words2 = new List<string> { "apple", "banana", "cherry" };
+
+        words.Add("date");
+        words.Add("elderberry");
+        words.Add("fig");
+
+        Console.WriteLine(words.Count);
+
+        foreach (string word in words)
+        {
+            Console.WriteLine(word);
+        }
     }
 }
