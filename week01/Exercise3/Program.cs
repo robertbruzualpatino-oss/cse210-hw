b2c87fc693012a16d3c07d4fa263232b6d8d6a05
robertbruzualpatino-oss <rbruzualpatio@byupathway.edu>
2026-01-06 19:14:37 -0400

    I've added sone more line statements in exercise 3.

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,7 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        // This is exercise 3
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("This is Exercise 3 of Week 1.");
+
+        string response = "yes";
+
+        while (response.ToLower() == "yes")
+        {
+            Console.Write("Do you want to continue? (yes/no): ");
+            response = Console.ReadLine();
+        }
+
+        string response2;
+
+        do
+        {
+            Console.Write("Do you want to continue? (yes/no): ");
+            response2 = Console.ReadLine();
+        } while (response.ToLower() == "yes");
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine(i);
+        }
+
+        string[] colors = { "Red", "Green", "Blue", "Yellow", "Purple"};
+        foreach (string color in colors)
+        {
+            Console.WriteLine(color);
+        }
+
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+        Console.WriteLine($"Random number between 1 and 100: {number}");
     }
 }
