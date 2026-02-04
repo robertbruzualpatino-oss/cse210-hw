26d95fa640c289ddf7a7ab3ebd6daebe86eac7ef
robertbruzualpatino-oss <rbruzualpatio@byupathway.edu>
2026-02-04 18:39:16 -0400

    I've inherited the 3 classes into the main program.

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment a1 = new Assignment("Alice", "Math");
+        Console.WriteLine(a1.GetSummary());
+
+        MathAssignment a2 = new MathAssignment("Bob", "Math", "5.1", "1-10");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+
+        WritingAssignment a3 = new WritingAssignment("Charlie", "English", "The Great Gatsby");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
     }
 }
