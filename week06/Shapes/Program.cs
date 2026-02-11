789722ebe8203c15ec46196d5ae6e0d4a146ff8f
robertbruzualpatino-oss <rbruzualpatio@byupathway.edu>
2026-02-11 15:39:17 -0400

    I've created a list of shapes of their respective colors, and their areas.

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,24 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>();
+
+        shapes.Add(new Square("Red", 5));
+        shapes.Add(new Rectangle("Blue", 4, 6));
+        shapes.Add(new Circle("Green", 3));
+
+        Console.WriteLine("--- Areas of the Shapes ---");
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+            double area = s.GetArea();
+
+            Console.WriteLine($"The shape in color {color} has an area of: {area:F2}");
+        }
     }
 }
