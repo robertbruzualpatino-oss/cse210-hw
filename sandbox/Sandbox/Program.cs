58742cea1a92ca2a618a16c36b6c019fac3e5f74
robertbruzualpatino-oss <rbruzualpatio@byupathway.edu>
2026-02-17 11:50:54 -0400

    I've created a Program in the Sandbox.

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,9 +1,45 @@
 using System;
+using System.Collections.Generic;
 
+public class Shape
+{
+    public string Name { get; set; }
+
+    public virtual void Draw()
+    {
+        Console.WriteLine($"Drawing a generic shape called: {Name}");
+    }
+}
+
+public class Circle : Shape
+{
+    public override void Draw()
+    {
+        Console.WriteLine($"Drawing a circle called: {Name}");
+    }
+}
+
+public class Rectangle : Shape
+{
+    public override void Draw()
+    {
+        Console.WriteLine($"Drwaing a rectangle called: {Name}"); 
+    }
+}
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! Welcome to the Sandbox project.");
+        List<Shape> myShapes = new List<Shape>
+        {
+            new Circle { Name = "Circle A" },
+            new Rectangle { Name = "Rectangle B" },
+            new Shape { Name = "Mysterious Shape" }
+        };
+
+        foreach (var shape in myShapes)
+        {
+            shape.Draw();
+        }
     }
 }
